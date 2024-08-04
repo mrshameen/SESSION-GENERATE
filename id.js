0155c0f058b2f7ac2adf7ff6c85fdfb1747b06d7
SL Shanuwa <Shameendurage@gmail.com>
2024-08-05 02:14:46 +0530

    ├✤ 𝑺𝑯𝑨𝑵𝑼𝑾𝑨 𝑴𝑰𝑵𝑫 ✤┤

--- id.js
+++ id.js
@@ -1,4 +1,4 @@
-𝑸function makeid(num = 4) {
+function makeid(num = 4) {
   let result = "";
   let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   var characters9 = characters.length;
